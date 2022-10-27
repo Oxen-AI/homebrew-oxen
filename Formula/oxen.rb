6e65444f79052c5070b7266fe19a523a869f78a9
Greg Schoeninger <greg.schoeninger@gmail.com>
2022-10-27 12:31:25 -0700

    release v0.2.0

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -1,9 +1,9 @@
 class Oxen < Formula
   desc "🐂 is a command line tool to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.1.1/oxen-mac.tar.gz"
-  sha256 "d2a668a12dbcc21a34239f28f38913573470622bbfd1969d6d99e0c3a44d951d"
-  version "0.1.1"
+  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.2.0/oxen-mac.tar.gz"
+  sha256 "bb9172da73fb652d6d1bf48372dd39e13596447ec50e273f74ca9978b1e70ee1"
+  version "0.2.0"
 
   def install
     bin.install "oxen"
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# homebrew-oxen
-Homebrew release formula for oxen
+# Oxen 🐂
+
+This is the formula for the homebrew release of Oxen.
+
+More detailed developer documentation can be found [here](https://github.com/Oxen-AI/oxen-release)
+
+```bash
+brew tap Oxen-AI/oxen
+brew install oxen
+```
