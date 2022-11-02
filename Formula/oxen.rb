17f8afa70c11cd1bec1e38f756c99566095395bd
Greg Schoeninger <greg.schoeninger@gmail.com>
2022-11-02 14:33:17 -0700

    update sha1 for release v0.2.1

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -2,7 +2,7 @@ class Oxen < Formula
   desc "🐂 is a command line tool to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
   url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.2.1/oxen-mac.tar.gz"
-  sha256 "c2c3a998579981c4ac145264d2185337a1621a7bb820771fbdb3fae54aa8b12b"
+  sha256 "0affb792f80f1a07f8e1bfcedf9fae1f0b52f8b948cf0387463bac7f5bec01af"
   version "0.2.1"
 
   def install
