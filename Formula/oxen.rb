6f15de02ddf4c3b59953e68103274539a8dfe9e7
Greg Schoeninger <greg.schoeninger@gmail.com>
2022-11-15 11:48:43 -0800

    bump v0.2.3

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -1,9 +1,9 @@
 class Oxen < Formula
   desc "🐂 is a command line tool to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.2.2/oxen-mac.tar.gz"
-  sha256 "50a017ccaa1e8fe2b1db5875c562fd55875400d963e02aed3d47af7a38536acc"
-  version "0.2.2"
+  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.2.3/oxen-mac.tar.gz"
+  sha256 "afb3d9cc16d684ae1bbc8216c7839802425a2ce22aa892ea1d4c20fda30123d7"
+  version "0.2.3"
 
   def install
     bin.install "oxen"
