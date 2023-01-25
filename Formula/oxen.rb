158038ef9f5bbdd9e5468e6c456e48d303795dec
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-01-24 22:07:45 -0800

    fix hash

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -2,7 +2,7 @@ class Oxen < Formula
   desc "🐂 is a command line tool to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
   url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.4.6/oxen-mac-0.4.6.tar.gz"
-  sha256 "6f8240aa34c3dec2c608d5afc5006a9d94f58d0c"
+  sha256 "ecf87156f53ceaf7126e5af0c3fd074cc260a9dce0e367b94f8ce4397f23e369"
   version "0.4.6"
 
   def install
