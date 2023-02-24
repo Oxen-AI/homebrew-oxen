92d747c49453a99a83ccaa7f998681d129161907
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-02-24 09:10:19 -0800

    bump v0.4.9

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -1,9 +1,20 @@
 class Oxen < Formula
-  desc "🐂 is a command line tool to version and manage large machine learning datasets"
+  desc "🐂 oxen is a command line tool to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.4.8/oxen-mac-0.4.8.tar.gz"
-  sha256 "dce7d5d3c334c9962e6a8477f7115c22ca3acab53bbff4aaddf219548e6f0716"
-  version "0.4.8"
+  version "0.4.9"
+
+  on_macos do
+    if MacOS.version >= :ventura # 13
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.4.9%2B1/oxen-mac-0.4.9.tar.gz"
+      sha256 "92927d35039466148387b3f7811ff0db783a174f85ebe852f7624191722624ff"
+    elsif MacOS.version >= :monterey # 12
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.4.9%2B1/oxen-mac-12.0-0.4.9+1.tar.gz"
+      sha256 "d839f911239227886a5e6d32786bd74e203c7db2fe9af5747a7bab3599f74078"
+    elsif MacOS.version >= :big_sur # 11
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.4.9%2B1/oxen-mac-11.0-0.4.9+1.tar.gz"
+      sha256 "d2636577082c4897ba8e300b45452392bdca4820d08f29755b267a09b7c11e55"
+    end
+  end
 
   def install
     bin.install "oxen"
