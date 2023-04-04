1149da0d85cf17710dab1efbc8a30a931c3952aa
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-04-04 10:14:30 -0700

    fix hash for oxen-mac-0.5.0.tar.gz

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -7,7 +7,7 @@ class Oxen < Formula
     if MacOS.version >= :ventura # 13
       if Hardware::CPU.arm?
         url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.0/oxen-mac-0.5.0.tar.gz"
-        sha256 "b3625476b6fd8d74383d4587f116deebf08f6334c1bf77165395f2e3b6fbbe06"
+        sha256 "55f4758198652fe2e74984d3d083d99ff0cd3a1d3011756a55353c9e9993c3dc"
       elsif Hardware::CPU.intel?
         url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.0/oxen-mac-x86_64-0.5.0.tar.gz"
         sha256 "64c431a75db1b0d3d89b0eb99fde4e488de55b1abc95b8181076efa5b35ee393"
