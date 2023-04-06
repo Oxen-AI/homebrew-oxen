83ed32229d41b98102f3e417bed15dd1d8dbc335
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-04-06 09:49:17 -0700

    bump v0.5.1

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -1,23 +1,23 @@
 class Oxen < Formula
   desc "🐂 oxen is a command line tool to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  version "0.5.0"
+  version "0.5.1"
 
   on_macos do
     if MacOS.version >= :ventura # 13
       if Hardware::CPU.arm?
-        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.0/oxen-mac-0.5.0.tar.gz"
-        sha256 "55f4758198652fe2e74984d3d083d99ff0cd3a1d3011756a55353c9e9993c3dc"
+        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-mac-0.5.1.tar.gz"
+        sha256 "02f06c5f6423d43cb8970c20498e67c359bad88e419a0bcccd4b7b16c1401bae"
       elsif Hardware::CPU.intel?
-        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.0/oxen-mac-x86_64-0.5.0.tar.gz"
-        sha256 "64c431a75db1b0d3d89b0eb99fde4e488de55b1abc95b8181076efa5b35ee393"
+        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-mac-x86_64-0.5.1.tar.gz"
+        sha256 "88dc7bc12c310e39d28f0120fdbb957287328030645f24d58df4f17a9483edd3"
       end
     elsif MacOS.version >= :monterey # 12
-      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.0/oxen-mac-12.0-0.5.0.tar.gz"
-      sha256 "2bd1ac3f7e7443ecd2b4647bd0ac4939a26d1edc3d850676564550b05189e646"
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-mac-12.0-0.5.1.tar.gz"
+      sha256 "92971810227a774d7ed32820ec743fbef5a20a6854478d8ecfc5a0ec5f1ac498"
     elsif MacOS.version >= :big_sur # 11
-      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.0/oxen-mac-11.0-0.5.0.tar.gz"
-      sha256 "b1586c8217bd9b7ebdccc54bbf7aba6113193a32679e76d519c76f64b57950db"
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-mac-11.0-0.5.1.tar.gz"
+      sha256 "6dd8c29f4228b34e2cdcdf587e3909082bd6fb451d26dd7551d1d9fb963e49c8"
     end
   end
 
--- compute_hashes.sh
+++ compute_hashes.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
-VERSION=0.5.0
+VERSION=$1
 
 echo "--CLI---"
 
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-$VERSION.tar.gz"
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-mac-$VERSION.tar.gz
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-x86_64-$VERSION.tar.gz"
+
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-x86_64-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-mac-x86_64-$VERSION.tar.gz
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-12.0-$VERSION.tar.gz"
+
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-12.0-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-mac-12.0-$VERSION.tar.gz
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-11.0-$VERSION.tar.gz"
+
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-mac-11.0-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-mac-11.0-$VERSION.tar.gz
 
 echo "--SERVER---"
 
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-$VERSION.tar.gz"
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-server-mac-$VERSION.tar.gz
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-x86_64-$VERSION.tar.gz"
+
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-x86_64-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-server-mac-x86_64-$VERSION.tar.gz
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-12.0-$VERSION.tar.gz"
+
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-12.0-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-server-mac-12.0-$VERSION.tar.gz
-wget -q "https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-11.0-$VERSION.tar.gz"
+
+URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-11.0-$VERSION.tar.gz"
+echo "Downloading $URL"
+wget -q $URL
 shasum -a 256 oxen-server-mac-11.0-$VERSION.tar.gz
