3671180050ac99879eff62410ff09415f8468020
Greg Schoeninger <gregschoeninger@gmail.com>
2023-05-30 14:52:35 -0700

    try release 0.6.1 with new github osx build

--- Formula/oxen.rb
+++ Formula/oxen.rb
@@ -1,23 +1,25 @@
 class Oxen < Formula
   desc "🐂 oxen is a command line tool to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  version "0.5.2"
+  version "0.6.1"
 
   on_macos do
     if MacOS.version >= :ventura # 13
-      if Hardware::CPU.arm?
-        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-mac-0.5.2.tar.gz"
-        sha256 "8ebbec9b0dc3bc39414fc0fddb2fef38037a0f2cbffe3004c1e055ab80fd076e"
-      elsif Hardware::CPU.intel?
-        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-mac-x86_64-0.5.2.tar.gz"
-        sha256 "e021bddf927746832e001da34d31a45f0dd6ffa423e681f522a8470b5bfcd2b6"
-      end
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.6.1/oxen-mac-13.0-0.6.1.tar.gz"
+      sha256 "b7a360cbbe8fb26ebe434eb8980b7ec5975f18d4661e05d01c5a41d0a8615901"
+      # if Hardware::CPU.arm?
+      #   url "https://github.com/Oxen-AI/Oxen/releases/download/v0.6.1/oxen-mac-0.6.1.tar.gz"
+      #   sha256 "8ebbec9b0dc3bc39414fc0fddb2fef38037a0f2cbffe3004c1e055ab80fd076e"
+      # elsif Hardware::CPU.intel?
+      #   url "https://github.com/Oxen-AI/Oxen/releases/download/v0.6.1/oxen-mac-x86_64-0.6.1.tar.gz"
+      #   sha256 "e021bddf927746832e001da34d31a45f0dd6ffa423e681f522a8470b5bfcd2b6"
+      # end
     elsif MacOS.version >= :monterey # 12
-      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-mac-12.0-0.5.2.tar.gz"
-      sha256 "8c133cc41e8edce8ac7a24fadf996361e7d30b66648b1360d3e4ccbacbab24a4"
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.6.1/oxen-mac-12.0-0.6.1.tar.gz"
+      sha256 "030b3929ef66ea978f82f8fe653d8c35e9ba67c5128ecc2d8cb794fcf8b07b8b"
     elsif MacOS.version >= :big_sur # 11
-      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-mac-11.0-0.5.2.tar.gz"
-      sha256 "0a24d14963d6db46785ef7fe3b7c23394c6f1ab8d179079c180f4d9bf6af8594"
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.6.1/oxen-mac-11.0-0.6.1.tar.gz"
+      sha256 "861aa56f2068e55bcce6d5af9bfc5e9048b63a2151aeaa3c68d1593b8d7b3268"
     end
   end
 
