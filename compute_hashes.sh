901e8ba1b20707e1028e7ae89f24e419c5db0575
eloymartinez <eloyalbmartinez@gmail.com>
2025-01-29 14:44:52 -0500

    fix workflow syntax

--- compute_hashes.sh
+++ compute_hashes.sh
@@ -10,21 +10,4 @@ for ver in 15.0 14.0 13.0; do
     echo "HASH_$ver=$(shasum -a 256 "oxen-mac-$ver.tar.gz" | awk '{print $1}')"
 done
 
-echo "[SERVER]"
-
-URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-15.0.tar.gz"
-echo "Downloading $URL"
-wget -q $URL
-shasum -a 256 oxen-server-mac-15.0.tar.gz
-
-URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-14.0.tar.gz"
-echo "Downloading $URL"
-wget -q $URL
-shasum -a 256 oxen-server-mac-14.0.tar.gz
-
-URL="https://github.com/Oxen-AI/Oxen/releases/download/v$VERSION/oxen-server-mac-13.0.tar.gz"
-echo "Downloading $URL"
-wget -q $URL
-shasum -a 256 oxen-server-mac-13.0.tar.gz
-
 rm *.tar.gz
